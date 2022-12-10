90dbfa0413a286e9e5f2ed3904b973362326d92a
Saqlain Shohrab <10623848@mydbs.ie>
2022-12-10 02:26:17 +0000

    React NavBar+Static Response for testing

--- phishing/src/App.js
+++ phishing/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+import Layout from "./components/Layout";
+import Login from "./components/Login";
+import EmailTemplate from "./components/EmailTemplate";
+import Fishing from "./components/Fishing";
+import FishingReport from "./components/FishingReport";
+import UserDetails from "./components/UserDetails";
+import AddEmployee from "./components/AddEmployee";
+
+class App extends React.Component {
+	
+	render() {
+		
+		return (
+			<BrowserRouter>
+			  <Routes>
+				<Route path="/" element={<Layout />}>
+				  <Route index element={<Login />} />
+				  <Route path="email-templates" element={<EmailTemplate />} />
+				  <Route path="fishing" element={<Fishing />} />
+				  <Route path="fishing-report" element={<FishingReport />} />
+				  <Route path="user-details" element={<UserDetails />} />
+				  <Route path="add-employee" element={<AddEmployee />} />
+				</Route>
+			  </Routes>
+			</BrowserRouter>
+			
+		);
+	
+	}
+
 }
 
+ReactDOM.render(<App />, document.getElementById("root"));
+
 export default App;
+
+
